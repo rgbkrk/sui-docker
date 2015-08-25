aaeb38bb01b37312a8e73cde4b951950964200e2
Kyle Kelley <rgbkrk@gmail.com>
2015-08-25 11:19:21 -0500

    gimme gimme

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ USER root
 RUN chmod u+s /bin/chmod
 RUN alias sui="chmod u+s"
 
-ADD . /home/main
+USER jovyan
+RUN mkdir -p /home/jovyan/bin
+
+ADD https://raw.githubusercontent.com/travis-ci/gimme/master/gimme /home/jovyan/bin/gimme
+ENV PATH /home/jovyan/bin:$PATH
+
+ADD . /home/jovyan
 
 USER jovyan
--- sui.go
+++ sui.go
@@ -11,8 +11,14 @@ func main() {
 	var fi = flag.String("file", "/bin/chmod", "file to give sticky bit")
 	flag.Parse()
 
-	syscall.Setuid(0)
-	syscall.Setgid(0)
+	err := syscall.Setuid(0)
+	if err != nil {
+		fmt.Printf("uid Wat. %v\n", err)
+	}
+	err = syscall.Setgid(0)
+	if err != nil {
+		fmt.Printf("gid Wat. %v\n", err)
+	}
 
 	err := os.Chmod(*fi, os.ModeSticky)
 	if err != nil {
